c3418b1c2e70ac81e8c3325a141365294f9ad06c
gitusp <usui@lemo.is>
2019-02-18 00:38:10 +0900

    Fix #2

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import flyd from "flyd";
-import takeUntil from "flyd/module/takeuntil";
 import { useState, useEffect } from "react";
 
 export const connect = (stream, deps) => {
   const [state, setState] = useState(stream());
   useEffect(() => {
-    const end = flyd.stream();
-    takeUntil(stream, end).map(setState);
+    const s = flyd.on(setState, stream);
     return () => {
-      end(true);
+      s.end(true);
     };
   }, deps);
   return state;
